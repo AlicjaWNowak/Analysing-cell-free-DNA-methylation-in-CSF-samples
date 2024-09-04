62a18247fc98ef6956d41b20ed92132bb7d304f1
Alicja <166818198+AlicjaWNowak@users.noreply.github.com>
2024-09-04 10:56:17 +0200

    Update FilteredCelfieOutput.R

--- FilteredCelfieOutput.R
+++ FilteredCelfieOutput.R
@@ -1,8 +1,13 @@
+#Example of CelFie output visualization
+#Melt input
+CelFie_loyfer_output<-read.delim("/path/to/Celfie_loyfer_output/1_tissue_proportions.txt", header = TRUE, sep = "\t", dec = ".") 
+CelFie_loyfer_output<-CelFie_loyfer_output %>% rename("samples"= "X") 
+CelFie_loyfer_output_melted <- melt(CelFie_loyfer_output, id.vars = "samples", variable.name = "tissue", value.name = "estimate")
 #Filtering the cell proportions
 filtered_data<-CelFie_loyfer_output_melted %>%
   filter(estimate >=0.01)
 #Load changed output with values beow treshold assigned to "other"
-filtered_CelFie_loyfer_output_melted<-read.csv("/Users/alicjanowak/Downloads/CelFie_filtered_loyfer_with_other.csv", header=TRUE)
+filtered_CelFie_loyfer_output_melted<-read.csv("path/to/elFie_filtered_loyfer_with_other.csv", header=TRUE)
 #Plot again
 ggplot(filtered_CelFie_loyfer_output_melted, aes(x = samples, y = estimate, fill = tissue)) +
   geom_bar(position = "stack", stat = "identity") +
